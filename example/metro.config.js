6fc60b065203413103399272f6bb2439d4ebd423
吴敬悦 <wutiange20@qq.com>
2024-10-27 10:32:16 +0800

    build: 在不直接添加依赖到example的情况下依赖自身

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,11 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require('path');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const pak = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+const modules = Object.keys({...pak.peerDependencies});
 
 /**
  * Metro configuration
@@ -8,11 +14,38 @@ const path = require('path');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  watchFolders: [path.resolve(__dirname, '..')],
+  watchFolders: [root],
+
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    extraNodeModules: {
-      '@wutiange/log-listener-plugin': path.resolve(__dirname, '..'),
-    },
+    blacklistRE: exclusionList(
+      modules.map(
+        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+      ),
+    ),
+
+    extraNodeModules: modules.reduce(
+      (acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      },
+      {
+        '@babel/runtime': path.resolve(
+          __dirname,
+          'node_modules/@babel/runtime',
+        ),
+      },
+    ),
+  },
+
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
 };
 
