f817c1efbb36a2abb86cb497ebcccc00d4698dbb
吴敬悦 <wutiange20@qq.com>
2024-12-02 00:05:04 +0800

    chore: 格式化代码

--- packages/example/index.js
+++ packages/example/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import logger from '@wutiange/log-listener-plugin';
 
-logger.config({isAuto: true});
+// 把 testUrl 更换成你自己的 IP 地址
+logger.config({isAuto: true, testUrl: '192.168.3.52'});
 
 AppRegistry.registerComponent(appName, () => App);
