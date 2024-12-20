4b0d1d5a964754a114f1a24fa94117c6b29bd0eb
wujingyue <jing.wu@seeed.cc>
2024-12-20 18:25:34 +0800

    fix: 因为历史的原因不是数组导致的错误

--- packages/log-listener-plugin/src/logPlugin.ts
+++ packages/log-listener-plugin/src/logPlugin.ts
@@ -1,7 +1,13 @@
 import Server from './Server';
 import { createClassWithErrorHandling } from './utils';
 import { httpInterceptor } from './HTTPInterceptor';
-import { DEFAULT_TIMEOUT, getDefaultStorage, Level, Tag, URLS_KEY } from './common';
+import {
+  DEFAULT_TIMEOUT,
+  getDefaultStorage,
+  Level,
+  Tag,
+  URLS_KEY,
+} from './common';
 import logger from './logger';
 
 type Options = {
@@ -9,67 +15,69 @@ type Options = {
    * storage 用于存储已设置的日志系统的 url
    * @default @react-native-async-storage/async-storage
    */
-  storage?: Storage
+  storage?: Storage;
   /**
    * 设置上传日志的超时时间，单位为毫秒
    * @default 3000
    */
-  timeout?: number
+  timeout?: number;
   /**
    * 日志系统的url
    */
-  testUrl?: string
+  testUrl?: string;
   /**
    * 是否自动开启日志记录
    * @default false
    */
-  isAuto?: boolean
+  isAuto?: boolean;
   /**
    * 设置日志系统的基础数据，这些数据会自动添加到每条日志中
    */
-  baseData?: Record<string, any>
-}
+  baseData?: Record<string, any>;
+};
 
 class LogPlugin {
   private server: Server | null = null;
   private timeout: number | null = null;
-  private isAuto = false
+  private isAuto = false;
   private storage: Storage | null = getDefaultStorage();
 
   constructor() {
-    this.init()
+    this.init();
   }
 
   private init = async () => {
     this.server = new Server();
 
     if (this.storage) {
-      const urlsStr = await this.storage.getItem(URLS_KEY)
+      const urlsStr = await this.storage.getItem(URLS_KEY);
       if (urlsStr) {
-        const urls = JSON.parse(urlsStr)
-        this.server.setBaseUrlArr(new Set(urls))
+        const urls = JSON.parse(urlsStr);
+        if (Array.isArray(urls)) {
+          this.server.setBaseUrlArr(new Set(urls));
+          httpInterceptor.setIgnoredUrls(this.handleIgnoredUrls());
+        }
       }
     }
 
-    
     this.server.addUrlsListener((urlArr) => {
       if (this.storage) {
-        this.storage.setItem(URLS_KEY, JSON.stringify(urlArr))
+        this.storage.setItem(URLS_KEY, JSON.stringify(urlArr));
       }
-      httpInterceptor.setIgnoredUrls(this.handleIgnoredUrls())
-    })
-  }
+      httpInterceptor.setIgnoredUrls(this.handleIgnoredUrls());
+    });
+  };
 
   config = ({ storage, timeout, testUrl, isAuto, baseData = {} }: Options) => {
     if (isAuto) {
-      this.auto()
+      this.auto();
     } else {
-      this.unAuto()
+      this.unAuto();
     }
     this.storage = storage ?? getDefaultStorage();
-    this.setTimeout(timeout ?? DEFAULT_TIMEOUT)
-    this.setBaseUrl(testUrl)
-    this.setBaseData(baseData)
+    this.setTimeout(timeout ?? DEFAULT_TIMEOUT);
+    this.setBaseUrl(testUrl);
+    this.setBaseData(baseData);
   };
 
   /**
@@ -78,98 +86,98 @@ class LogPlugin {
   auto = () => {
     this.startRecordNetwork();
     this.startRecordLog();
-    this.isAuto = true
-  }
+    this.isAuto = true;
+  };
 
   unAuto = () => {
-    this.stopRecordLog()
-    httpInterceptor.disable()
-    httpInterceptor.removeAllListener()
-    this.isAuto = false
-  }
+    this.stopRecordLog();
+    httpInterceptor.disable();
+    httpInterceptor.removeAllListener();
+    this.isAuto = false;
+  };
 
   startRecordLog = () => {
     console.log = (...data: any[]) => {
       this.log(...data);
       if (!__DEV__) {
-        return
+        return;
       }
-      logger.log(...data)
+      logger.log(...data);
     };
 
     console.warn = (...data: any[]) => {
       this.warn(...data);
       if (!__DEV__) {
-        return
+        return;
       }
-      logger.warn(...data)
+      logger.warn(...data);
     };
 
     console.error = (...data: any[]) => {
       this.error(...data);
       if (!__DEV__) {
-        return
+        return;
       }
-      logger.error(...data)
+      logger.error(...data);
     };
-  }
+  };
 
   stopRecordLog = () => {
-    console.log = logger.log
-    console.warn = logger.warn
-    console.error = logger.error
-  }
+    console.log = logger.log;
+    console.warn = logger.warn;
+    console.error = logger.error;
+  };
 
   private handleIgnoredUrls = () => {
-    const urls = this.server?.getBaseUrlArr?.()
-    const ignoredUrls: string[] = []
+    const urls = this.server?.getBaseUrlArr?.();
+    const ignoredUrls: string[] = [];
     if (urls?.size) {
       urls.forEach((url) => {
-        ignoredUrls.push(`${url}/log`, `${url}/network`, `${url}/join`)
-      })
+        ignoredUrls.push(`${url}/log`, `${url}/network`, `${url}/join`);
+      });
     }
-    return ignoredUrls
-  }
+    return ignoredUrls;
+  };
 
   startRecordNetwork = () => {
-    httpInterceptor.addListener("send", (data) => {
+    httpInterceptor.addListener('send', (data) => {
       this.server?.network({
         url: data.url,
         id: data.id,
         method: data.method,
         headers: data.requestHeaders,
         body: data.requestData,
-        createTime: data.startTime
-      })
-    })
-    httpInterceptor.addListener("response", (data) => {
+        createTime: data.startTime,
+      });
+    });
+    httpInterceptor.addListener('response', (data) => {
       this.server?.network({
         headers: data.responseHeaders,
         body: data.responseData,
         requestId: data.id,
         statusCode: data.status,
-        endTime: data.endTime
-      })
-    })
-    httpInterceptor.enable({ignoredUrls: this.handleIgnoredUrls()})
-  }
+        endTime: data.endTime,
+      });
+    });
+    httpInterceptor.enable({ ignoredUrls: this.handleIgnoredUrls() });
+  };
 
   /**
    * @deprecated 这个方法将在下一个主要版本中被移除。请使用 config({testUrl: ''}) 替代。
    */
   setBaseUrl = (url?: string) => {
-    const tempUrl = url?.trim()
+    const tempUrl = url?.trim();
     if (this.server) {
       this.server.updateUrl(tempUrl);
     } else {
       this.server = new Server(tempUrl);
     }
-    httpInterceptor.setIgnoredUrls(this.handleIgnoredUrls())
+    httpInterceptor.setIgnoredUrls(this.handleIgnoredUrls());
     if (this.isAuto) {
       this.startRecordNetwork();
-      this.startRecordLog()
+      this.startRecordLog();
     }
-  }
+  };
 
   /**
    * @deprecated 这个方法将在下一个主要版本中被移除。请使用 config({timeout: 3000}) 替代。
@@ -179,7 +187,7 @@ class LogPlugin {
       this.timeout = timeout;
       this.server?.updateTimeout(this.timeout);
     }
-  }
+  };
 
   /**
    * @deprecated 这个方法将在下一个主要版本中被移除。移除后将不再支持获取超时时间。
@@ -189,14 +197,14 @@ class LogPlugin {
       return this.timeout;
     }
     return null;
-  }
+  };
 
   /**
    * @deprecated 这个方法将在下一个主要版本中被移除。请使用 config({baseData: {}}) 替代。
    */
   setBaseData = (data: Record<string, any> = {}) => {
-    this.server?.updateBaseData(data)
-  }
+    this.server?.updateBaseData(data);
+  };
 
   private _log = (level: string, tag: string, ...data: any[]) => {
     const sendData = {
@@ -206,26 +214,25 @@ class LogPlugin {
       createTime: Date.now(),
     };
     this.server?.log(sendData);
-  }
+  };
 
   tag = (tag: string, ...data: any[]) => {
     this._log(Level.LOG, tag, ...data);
-  }
+  };
 
   log = (...data: any[]) => {
     this._log(Level.LOG, Tag.DEFAULT, ...data);
-  }
+  };
 
   warn = (...data: any[]) => {
     this._log(Level.WARN, Tag.DEFAULT, ...data);
-  }
+  };
 
   error = (...data: any[]) => {
     this._log(Level.ERROR, Tag.DEFAULT, ...data);
-  }
-
+  };
 }
-const SafeLogPlugin = createClassWithErrorHandling(LogPlugin)
+const SafeLogPlugin = createClassWithErrorHandling(LogPlugin);
 const logPlugin = new SafeLogPlugin();
 export { SafeLogPlugin };
 export default logPlugin;
