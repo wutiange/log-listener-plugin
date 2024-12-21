1676048c687d834ab0d47b94c40ac1e6d7148dd4
wujingyue <jing.wu@seeed.cc>
2024-12-21 09:55:53 +0800

    build: rollup ts 打包修改

--- packages/log-listener-plugin/rollup.config.mjs
+++ packages/log-listener-plugin/rollup.config.mjs
@@ -1,4 +1,9 @@
 import { createConfig } from '../../rollup.config.base.mjs';
 import packageJson from './package.json' assert { type: 'json' };
 
-export default createConfig(packageJson);
+export default createConfig(packageJson, {
+  external: [
+    'react-native/Libraries/Network/XHRInterceptor',
+    'react-native/Libraries/Blob/FileReader',
+  ],
+});
--- rollup.config.base.mjs
+++ rollup.config.base.mjs
@@ -1,14 +1,14 @@
-import { fileURLToPath } from "url";
-import typescript from "@rollup/plugin-typescript";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import dts from "rollup-plugin-dts";
-import { dirname, resolve } from "path";
+import { fileURLToPath } from 'url';
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+import { dirname, resolve } from 'path';
 
 // 正确获取 __dirname 的替代方案
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const input = "index.ts";
+const input = 'index.ts';
 
 export function createConfig(pkg, options = {}) {
   return [
@@ -17,7 +17,7 @@ export function createConfig(pkg, options = {}) {
       input,
       output: {
         file: pkg.types,
-        format: "es",
+        format: 'es',
       },
       plugins: [dts()],
     },
@@ -27,24 +27,26 @@ export function createConfig(pkg, options = {}) {
       output: [
         {
           file: pkg.main,
-          format: "cjs",
+          format: 'cjs',
           sourcemap: true,
         },
         {
           file: pkg.module,
-          format: "es",
+          format: 'es',
           sourcemap: true,
         },
       ],
       plugins: [
         typescript({
-          tsconfig: resolve(__dirname, "./tsconfig.json"),
+          tsconfig: resolve(__dirname, './tsconfig.json'),
           declaration: false,
           compilerOptions: {
-            outDir: "dist",
-          }
+            outDir: 'dist',
+          },
         }),
-        nodeResolve(),
+        nodeResolve({
+          extensions: ['.ts', '.js'],
+        }),
         commonjs(),
       ],
       ...options,
